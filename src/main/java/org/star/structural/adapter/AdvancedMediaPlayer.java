702475dd4fc1d6e9d781d5804788abc2c2365058
invokerwbw <wbw911@126.com>
2018-08-03 11:09:43 +0800

    Add Adapter Pattern

---
+++ src/main/java/org/star/structural/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package org.star.structural.adapter;
+
+public interface AdvancedMediaPlayer {
+
+    void playVlc(String fileName);
+
+    void playMp4(String fileName);
+
+}
---
+++ src/main/java/org/star/structural/adapter/MediaPlayer.java
@@ -0,0 +1,7 @@
+package org.star.structural.adapter;
+
+public interface MediaPlayer {
+
+    void play(String audioType, String fileName);
+
+}
